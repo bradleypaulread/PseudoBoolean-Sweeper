19d3c232eac4e32e5f1be957b971aca3bdddf470
Bradley Paul Read <b.read@ncl.ac.uk>
2019-02-04 23:28:16 +0000

    Finally got PB to work. Was a wrong import.

--- src/SATSandbox.java
+++ src/SATSandbox.java
@@ -3,13 +3,22 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.sat4j.minisat.SolverFactory;
+import java.math.BigInteger;
+
+import org.sat4j.core.Vec;
+import org.sat4j.core.VecInt;
+import org.sat4j.pb.IPBSolver;
+import org.sat4j.pb.OptToPBSATAdapter;
+import org.sat4j.pb.PseudoOptDecorator;
+import org.sat4j.pb.SolverFactory;
 import org.sat4j.reader.InstanceReader;
 import org.sat4j.reader.ParseFormatException;
 import org.sat4j.reader.Reader;
 import org.sat4j.specs.ContradictionException;
 import org.sat4j.specs.IProblem;
 import org.sat4j.specs.ISolver;
+import org.sat4j.specs.IVec;
+import org.sat4j.specs.IVecInt;
 import org.sat4j.specs.TimeoutException;
 import org.sat4j.tools.ModelIterator;
 
@@ -17,40 +26,73 @@ public class SATSandbox {
 
 	public static void main(String[] args) throws ContradictionException, TimeoutException, IOException {
 
-		ISolver solver = SolverFactory.newDefault();
-		ModelIterator mi = new ModelIterator(solver);
-		solver.setTimeout(3600); // 1 hour timeout
-		Reader reader = new InstanceReader(mi);
-		// filename is given on the command line
+		// PB Plaything
+		
+		IPBSolver pbSolver = SolverFactory.newDefault();		
+		IVecInt lits = new VecInt();
+		lits.push(1);
+		lits.push(2);
+		lits.push(-3);
+		lits.push(-4);
+		IVec<BigInteger> coeffs = new Vec<BigInteger>();
+		coeffs.push(BigInteger.valueOf(1));
+		coeffs.push(BigInteger.valueOf(1));
+		coeffs.push(BigInteger.valueOf(1));
+		coeffs.push(BigInteger.valueOf(1));
+		pbSolver.addExactly(lits, coeffs, BigInteger.valueOf(2));
+		
+		OptToPBSATAdapter optimizer = new OptToPBSATAdapter(new PseudoOptDecorator(pbSolver));
 
-		List<int[]> results = new ArrayList<int[]>();
-		// PBSolverClause pb = new PBSolverClause();
-		try {
-			boolean unsat = true;
-			IProblem problem = reader.parseInstance("resources/queens16.cnf");
-			while (problem.isSatisfiable()) {
-				unsat = false;
-				int[] model = problem.model();
-				// do something with each model
-				results.add(model);
+		while (optimizer.isSatisfiable()) {
+			System.out.println("SATISFIABLE!");
+			int[] model = optimizer.model();
+			lits.clear();
+			for (int i : model) {
+				System.out.print("" + i + ", ");
+				lits.push(i*-1);
 			}
-			System.out.println("\n\n\nNumber of solutions: " + results.size());
-
-			if (unsat) {
-				// do something for unsat case
-				System.out.println("\n\n\nNumber of solutions: " + results.size());
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (ParseFormatException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ContradictionException e) {
-			System.out.println("Unsatisfiable (trivial)!");
-		} catch (TimeoutException e) {
-			System.out.println("Timeout, sorry!");
+			pbSolver.addExactly(lits, coeffs, BigInteger.valueOf(2));
+			optimizer = new OptToPBSATAdapter(new PseudoOptDecorator(pbSolver));
+			System.out.println("\n");
 		}
+		if (!optimizer.isSatisfiable()) {
+			System.out.println("test");
+		}
+		
+//		ISolver solver = SolverFactory.newDefault();
+//		ModelIterator mi = new ModelIterator(solver);
+//		solver.setTimeout(3600); // 1 hour timeout
+//		Reader reader = new InstanceReader(mi);
+//		// filename is given on the command line
+//
+//		List<int[]> results = new ArrayList<int[]>();
+//		// PBSolverClause pb = new PBSolverClause();
+//		try {
+//			boolean unsat = true;
+//			IProblem problem = reader.parseInstance("resources/queens16.cnf");
+//			while (problem.isSatisfiable()) {
+//				unsat = false;
+//				int[] model = problem.model();
+//				// do something with each model
+//				results.add(model);
+//			}
+//			System.out.println("\n\n\nNumber of solutions: " + results.size());
+//
+//			if (unsat) {
+//				// do something for unsat case
+//				System.out.println("\n\n\nNumber of solutions: " + results.size());
+//			}
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (ParseFormatException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} catch (ContradictionException e) {
+//			System.out.println("Unsatisfiable (trivial)!");
+//		} catch (TimeoutException e) {
+//			System.out.println("Timeout, sorry!");
+//		}
 
 		// ISolver solver = SolverFactory.newDefault();
 		// solver.setTimeout(3600); // 1 hour timeout
