8e1239bd2041375e730ad3f45b6fc855ca00df7c
bradleypaulread <bradleypaulread@gmail.com>
2020-06-10 16:40:29 +0100

    Renamed class for better self explanatory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,7 @@ package main.java;
 
 import main.java.gui.GameFrame;
 
-/**
- * A launcher class for a game of PseudoBoolean-Sweeper.
- *
- * @author Bradley Read
- * @version 1.0
- * @since 2019-08-30
- */
-public class GameLauncher {
+public class Main {
     public static void main(String[] args) {
         MineSweeper game = new MineSweeper(Difficulty.BEGINNER);
         GameFrame gui = new GameFrame(game);
